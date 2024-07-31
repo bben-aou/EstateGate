00268db7dee9c832084ffb4af0f490f291b4947b
Bilal Ben Aouad <bilal.benaouad@yakeey.com>
2024-07-31 22:07:19 +0100

    chore : add  tailwind configuration and setup colors

---
+++ client/tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors : {
+        light : {
+          0 : "#FFFFFF",
+          5:  "#FCFCFD",
+          10 : "#fcfcfe",
+          20 : "#fbfbfd",
+          30 : "#EEEEEF",
+          40 : "#64656A",
+          50 : "#dfd5f4",
+          60 : "#4564d5",
+          70 : "#785cb3",
+        },
+        dark : {
+          10 : "#1e1a3d",
+          20 : "#000000"
+        }
+      }
+    },
+  },
+  plugins: [],
+}
