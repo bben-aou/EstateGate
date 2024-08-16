fe7854214330d33d564109e62fe1c507b3349062
Bilal Ben Aouad <bilal.benaouad@yakeey.com>
2024-08-16 20:40:50 +0100

    feat (header/hamburgerMenu) : add UI and logic of header with navigation actions for sm/md/lg screens

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -1,12 +1,19 @@
 import { FormattedMessage, useIntl } from "react-intl";
-import { navItems, TNavItems } from "@components/header/navigationItems";
+import { navItems } from "@components/header/navigationItems";
 import { Link, NavLink } from "react-router-dom";
 import LoginButton from "@components/buttons/LoginButton";
 import SignUpButton from "@components/buttons/SignUpButton";
 import { RiMenu5Fill as OpenMenuIcon } from "react-icons/ri";
+import { Fragment } from "react/jsx-runtime";
+import HamburgerMenu from "@components/header/hamburgerMenu/HamburgerMenu";
+import { TNavItems } from "@interfaces/index";
+import { useHeaderContext } from "@global/states/hooks/useHeaderContext";
+import ConditionalRendering from "@components/common/ConditionalRendering";
+
 
 const Header = () => {
   const intl = useIntl();
+  const {isHamburgerMenuOpen, setIsHamburgerMenuOpen} = useHeaderContext();
   const navigationItems = navItems.map((navItem: TNavItems) => {
     return (
       <li key={navItem.id}>
@@ -29,28 +36,38 @@ const Header = () => {
     );
   });
   return (
-    <header className="w-full h-[8vh] md:flex md:items-center md:justify-between px-[25px] md:px-[70px] border-b-[1px] border-light-30">
-      <nav className="h-full flex justify-between items-center md:flex-row md:justify-start md:gap-[100px]">
-        <Link
-          to={"/"}
-          className="font-inter font-semibold text-[22px] cursor-pointer text-light-60"
-        >
-          {intl.formatMessage({ id : 'logoTitle'})}
-        </Link>
-        <OpenMenuIcon className=" md:hidden w-[22px] h-[22px] text-light-60"/>
-        <ul className=" hidden md:flex items-center gap-[30px]">{navigationItems}</ul>
-      </nav>
-      <div className="hidden md:flex h-full  items-center gap-[20px]">
-        <LoginButton
+    <Fragment>
+      <header className="absolute w-full h-[8vh] md:flex md:items-center md:justify-between px-[25px] lg:px-[70px] border-b-[1px] border-light-30">
+        <nav className="h-full flex justify-between items-center md:flex-row md:justify-start md:gap-[35px] lg:gap-[100px]">
+          <Link
+            to={"/"}
+            className="md:hidden lg:flex font-inter font-semibold text-[22px] cursor-pointer text-light-60"
+          >
+            {intl.formatMessage({ id: "logoTitle" })}
+          </Link>
+          <div className="h-full flex items-center">
+            <img src="EstateGateLogo.svg" alt="logo" className="w-[40px] hidden md:flex lg:hidden"/>
+          </div>
+          <OpenMenuIcon className=" md:hidden w-[22px] h-[22px] text-light-60" onClick={() => setIsHamburgerMenuOpen(true)}/>
+          <ul className="hidden md:flex items-center lg:gap-[30px]">
+            {navigationItems}
+          </ul>
+        </nav>
+        <div className="hidden md:flex h-full  items-center md:gap-[5px] lg:gap-[20px]">
+          <LoginButton
             label={intl.formatMessage({ id: "authenticationLogin" })}
             redirectionPath={"/login"}
-        />
-       <SignUpButton
+          />
+          <SignUpButton
             label={intl.formatMessage({ id: "authenticationSignUp" })}
             redirectionPath={"/sign-up"}
-       />
-      </div>
-    </header>
+          />
+        </div>
+      </header>
+      <ConditionalRendering condition={isHamburgerMenuOpen}>
+        <HamburgerMenu/>
+      </ConditionalRendering>
+    </Fragment>
   );
 };
 export default Header;
--- client/src/components/header/navigationItems.ts
+++ client/src/components/header/navigationItems.ts
@@ -1,14 +1,14 @@
-export type TNavItems = {
-    id : number ;
-    messageId : string;
-    defaultMessage : string;
-    path : string;
-}
+import { HiHome } from "react-icons/hi2";
+import { PiNewspaperFill } from "react-icons/pi";
+import { BiSolidMessageSquareDetail } from "react-icons/bi";
+import { MdRealEstateAgent } from "react-icons/md";
+import { TNavItems } from "@interfaces/index";
+
 
 
 export const navItems : TNavItems[] = [
-    {id : 1, messageId : "menuItemsHome" , defaultMessage: "Home", path : "/"},
-    {id : 2, messageId : "menuItemsAbout" , defaultMessage: "About", path : "/about"},
-    {id : 3, messageId : "menuItemsContact" , defaultMessage: "Contact", path : "/contact"},
-    {id : 3, messageId : "menuItemsAgents" , defaultMessage: "Agents", path : "/agents"},
+    {id : 1, messageId : "menuItemsHome" , defaultMessage: "Home", path : "/", Icon : HiHome},
+    {id : 2, messageId : "menuItemsAbout" , defaultMessage: "About", path : "/about", Icon : PiNewspaperFill},
+    {id : 3, messageId : "menuItemsContact" , defaultMessage: "Contact", path : "/contact", Icon : BiSolidMessageSquareDetail},
+    {id : 4, messageId : "menuItemsAgents" , defaultMessage: "Agents", path : "/agents", Icon : MdRealEstateAgent},
 ]
