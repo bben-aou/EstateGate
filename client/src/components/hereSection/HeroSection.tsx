438e36cc19824bc59afcecfb253e36aa3cb01de5
Bilal Ben Aouad <bilal.benaouad@yakeey.com>
2024-09-06 22:22:18 +0100

    refactor : update UI structure and adapt component responsivity

--- client/src/components/hereSection/HeroSection.tsx
+++ client/src/components/hereSection/HeroSection.tsx
@@ -13,19 +13,17 @@ const HeroSection = () => {
         />
       </div>
 
-      <div className="h-1/2 lg:h-full w-full lg:w-[50%]  flex flex-col px-[45px] py-[91px] text-light-5 ">
-        <h1>{intl.formatMessage({ id: "home.hero.section.greeting" })}</h1>
+      <div className="h-1/2 lg:h-full w-full lg:w-[50%] flex flex-col px-[45px] py-[91px] text-light-5 ">
+        <h1 className="text-sm lg:text-[20px]">{intl.formatMessage({ id: "home.hero.section.greeting" })}</h1>
         <h1 className="font-bold lg:text-[45px] text-[35px] my-[5px] lg:my-[10px] ">
           {intl.formatMessage({ id: "home.hero.section.title" })}
         </h1>
-        <p className="text-[14px] mb-[25px]">
+        <p className="text-sm lg:text-[16px] mb-[25px]">
           {intl.formatMessage({ id: "home.hero.section.info" })}
         </p>
-        <div className="hidden lg:flex">
-          <Filter />
-        </div>
+        <Filter containerStyle="hidden lg:flex"/>
       </div>
-      <div className="lg:hidden  px-[45px] h-1/2 flex items-center ">
+      <div className="lg:hidden px-[45px] h-1/2 flex items-center">
         <Filter />
       </div>
     </div>
