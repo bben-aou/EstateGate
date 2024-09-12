16945ea95f9aa80f924e54adfbb469d1ffe53bf6
Bilal Ben Aouad <bilal.benaouad@yakeey.com>
2024-09-12 22:05:47 +0100

    feat : add login types for sign in and sign up pages

---
+++ client/src/interfaces/components/login/sign-in/types.ts
@@ -0,0 +1,12 @@
+export type TLoginImage = {
+    imgSrc: string;
+    imgAlt: string;
+    imgClassName?: string;
+    wrapperClassName?: string;
+    containerClassName?: string;
+  };
+
+export type TSignInHeader = {
+    greetingMessage: string;
+    promptMessage: string;
+  };
