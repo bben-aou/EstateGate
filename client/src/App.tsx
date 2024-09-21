8250101fb2a8b0d987002aea3e5116b45b0b5b83
Bilal Ben Aouad <bilal.benaouad@yakeey.com>
2024-09-21 11:55:10 +0100

    feat (Routing) : add AuthGuard component to protect the private routes that needs the authentication

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -14,16 +14,17 @@ import { Toaster } from "@components/ui/toaster";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import SignUp from "@/pages/login/sign-up/SignUp";
 import { AuthProvider } from "@/providers/AuthProvider";
-// import PrivateRoute from "./components/privateRoute/PrivateRoute";
+import Profile from "@/pages/profile/Profile";
+import AuthGuard from "@/components/privateRoute/AuthGuard";
 
 function App() {
   const [locale] = useState<Locale>("en");
   const queryClient = new QueryClient();
 
   return (
-    <AuthProvider>
-      <IntlManager locale={locale}>
-        <QueryClientProvider client={queryClient}>
+    <IntlManager locale={locale}>
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
           <Router>
             <Toaster />
             <div className="bg-light-5 h-screen w-screen font-inter absolute">
@@ -34,24 +35,35 @@ function App() {
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
                 <Route path="/contact" element={<Contact />} />
-                {/* <Route 
-                  path="/agents" 
+
+                {/* Public routes */}
+                <Route
                   element={
-                    <PrivateRoute>
-                      <Agents />
-                    </PrivateRoute>
-                  } 
-                /> */}
-                <Route path="/agents" element={<Agents />} />
-                <Route path="/login/sign-in" element={<Login />} />
-                <Route path="/login/sign-up" element={<SignUp />} />
+                    <AuthGuard requireAuth={false} redirectTo="/profile" />
+                  }
+                >
+                  <Route path="/login/sign-in" element={<Login />} />
+                  <Route path="/login/sign-up" element={<SignUp />} />
+                </Route>
+
+                {/* Protected routes */}
+                <Route
+                  element={
+                    <AuthGuard requireAuth={true} redirectTo="/login/sign-in" />
+                  }
+                >
+                  <Route path="/agents" element={<Agents />} />
+                  <Route path="/profile" element={<Profile />} />
+                  <Route path="/profile/:userId" element={<Profile />} />
+                </Route>
+
                 <Route path="*" element={<NotFound />} />
               </Routes>
             </div>
           </Router>
-        </QueryClientProvider>
-      </IntlManager>
-    </AuthProvider>
+        </AuthProvider>
+      </QueryClientProvider>
+    </IntlManager>
   );
 }
 
