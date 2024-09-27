67791802bdacae7dad5f0499412e7730dfeeb838
Bilal Ben Aouad <bilal.benaouad@yakeey.com>
2024-09-27 19:13:50 +0100

    feat (Backend) : add logic implementation of google authentication and add new columns in User Entity to serve the new profile information in the frontend

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,19 +3,46 @@ import bodyParser from "body-parser";
 import routes from "@routes/index";
 import authRoute from "@routes/auth.route";
 import cookieParser from "cookie-parser";
-import cors from "cors";
+import cors, { CorsOptions } from "cors";
 import dotenv from "dotenv";
+import session from "express-session";
+import passport from "passport";
+import { setupGoogleStrategy } from "@config/googleStrategy";
+
+
 
 const app = express();
 
 dotenv.config();
+app.use(session({
+    secret: process.env.SESSION_SECRET || 'secret',
+    resave: false,
+    saveUninitialized : true,
+}))
+
+app.use(passport.initialize());
+setupGoogleStrategy();
 
 app.use(bodyParser.json());
 app.use(cookieParser());
 const CLIENT_URL = process.env.CLIENT_URL;
-app.use(cors({ origin: CLIENT_URL, credentials: true }));
+
+const corsOptions: CorsOptions = {
+    origin: (origin, callback) => {
+      const allowedOrigins = [process.env.CLIENT_URL, 'https://accounts.google.com'].filter(Boolean);
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true
+  };
+  
+  app.use(cors(corsOptions));
+
 
 app.use("/api", routes);
-app.use("/api/auth", authRoute);
+app.use("/api/auth",authRoute);
 
 export default app;
--- server/src/routes/auth.route.ts
+++ server/src/routes/auth.route.ts
@@ -1,12 +1,16 @@
-import { Router } from 'express'
-import { login, logout, me, refreshToken, register } from '@controllers/auth.controller';
+import { Router } from "express";
+import { googleAuth, googleAuthCallback, login, logout, me , refreshToken, register } from '@controllers/auth.controller';
 
 const router = Router();
+
 
 router.post('/register',register);
 router.post('/login', login);
 router.post('/logout', logout);
 router.post('/refresh-token',refreshToken)
 router.get('/me',me)
+//Google Authentication routes
+router.get('/google', googleAuth);
+router.get('/google/callback', googleAuthCallback);
 
 export default router;
