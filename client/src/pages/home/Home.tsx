77404d162f2f528df9960fe70649be6ad6497ee7
Bilal Ben Aouad <bilal.benaouad@yakeey.com>
2024-10-04 18:40:37 +0100

    feat : use the new sections in the home page

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -1,11 +1,14 @@
-import Statistics from "@components/statistics/Statistics";
 import HeroSection from "@components/hereSection/HeroSection";
-
+import AdvantagesSection from "@/components/advantages/AdvantagesSection";
+import SupportDescriptionSection from "@/components/supportDescription/SupportDescriptionSection";
+import BasedOnYourLocationSection from "@/components/basedOnYourLocation/BasedOnYourLocationSection";
 const Home = () => {
   return (
-    <div className="md:mx-[10%] mt-[8vh] h-[92vh] mb-[30px] flex flex-col justify-evenly ">
-     <HeroSection/>
-      <Statistics />
+    <div className="bg-light-20">
+      <HeroSection />
+      <AdvantagesSection />
+      <SupportDescriptionSection/>
+      <BasedOnYourLocationSection/>
     </div>
   );
 };
